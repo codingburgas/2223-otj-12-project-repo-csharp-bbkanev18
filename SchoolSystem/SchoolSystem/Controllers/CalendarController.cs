dbcb8faca65ebedd695c064dc7c4369679c0855d
bbkanev18 <bbkanev18@codingburgas.bg>
2023-05-09 14:38:48 +0300

    Add Calendar to the project.

---
+++ SchoolSystem/SchoolSystem/Controllers/CalendarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SchoolSystem.DAL.DataTransferObjects;
+using SchoolSystem.DAL.Models;
+
+namespace SchoolSystem.Controllers
+{
+    [Authorize(Roles ="user,teacher,admin")]
+    public class CalendarController : Controller
+    {
+        private readonly SchoolDBContext _schoolDBContext;
+        public CalendarController(SchoolDBContext schoolDBContext)
+        {
+            _schoolDBContext = schoolDBContext;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new List<CalendarTransferObject>();
+            var newEvent = new CalendarTransferObject
+            {
+                Name = "New Event",
+                Deadline = DateTime.Now
+            };
+            model.Add(newEvent);
+            return View(model);
+        }
+    }
+}
